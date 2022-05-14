40bef5d3afd05c48d44b4afc4035eba0c1bf8467
malisambhaji24 <85444584+malisambhaji24@users.noreply.github.com>
2022-05-14 13:31:07 +0530

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Hello world!</title>
+	<title>Hello Techverito!</title>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
@@ -22,7 +22,7 @@
 	// echo "</pre>";
 ?>
 	<img id="logo" src="logo.png" />
-	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
+	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"TechVerito")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
