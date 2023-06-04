5165373b826e99b1211b6ac6b4bbd262d8bdf4cb
malisambhaji24 <85444584+malisambhaji24@users.noreply.github.com>
2023-06-04 16:41:12 +0530

    Update index.php

--- www/index.php
+++ www/index.php
@@ -22,7 +22,7 @@
 	// echo "</pre>";
 ?>
 	<img id="logo" src="logo.png" />
-	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"TechVerito")."!"; ?></h1>
+	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"Kiran Mali")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
